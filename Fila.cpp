369915233418e6302342c81212f3e841dc102506
OnNet Telecom <danielgjjpatrocinio@gmail.com>
2022-05-17 14:06:50 -0300

    Pilha,Fila,Lista

---
+++ Fila.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<iostream>
+#include<queue>
+
+using namespace std;
+int main () {
+  queue<int> fila;
+
+  // inserir elementos
+  fila.push(10);
+  fila.push(15);
+  fila.push(20);
+  fila.push(50);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}return 0;
